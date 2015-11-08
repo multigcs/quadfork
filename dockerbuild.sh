7c7922dfcbe11de460fd49d393eceba5e19913c4
Oliver Dippel <oliver@multixmedia.org>
2015-11-08 18:11:11 +0100

    fix dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -6,7 +6,7 @@ DIR="`pwd`"
 
 mkdir -p build
 
-cat <<EOF > build/Dockerfile
+cat <<EOF > Dockerfile
 FROM debian:jessie
 ENV DEBIAN_FRONTEND noninteractive
 RUN sed -i "s|^deb-src|#deb-src|g" /etc/apt/sources.list
@@ -16,22 +16,22 @@ RUN apt-get install -y git make gcc-arm-none-eabi dfu-util stm32flash
 EOF
 if test -d ./src/
 then
-	echo "RUN mkdir -p /usr/src/quadfork" >> build/Dockerfile
+	echo "RUN mkdir -p /usr/src/quadfork" >> Dockerfile
 	for PART in Makefile Libraries src
 	do
-		echo "COPY \"$DIR/$PART\" \"/usr/src/quadfork/$PART\"" >> build/Dockerfile
+		echo "COPY \"$PART\" \"/usr/src/quadfork/$PART\"" >> Dockerfile
 	done
 else
-	echo "RUN (cd /usr/src/ ; git clone https://github.com/multigcs/quadfork.git)" >> build/Dockerfile
+	echo "RUN (cd /usr/src/ ; git clone https://github.com/multigcs/quadfork.git)" >> Dockerfile
 fi
 
 echo "#!/bin/sh" > build/build.sh
 echo "(cd /usr/src/quadfork ; make \$@)" >> build/build.sh
 chmod 755 build/build.sh
 
-docker build -t quadfork build
+docker build -t quadfork .
 docker run --privileged=true -i -t -v "$DIR/build":/usr/src/quadfork/build quadfork /bin/bash /usr/src/quadfork/build/build.sh $@
 
-rm -rf build/Dockerfile
+rm -rf Dockerfile
 rm -rf build/build.sh
 
