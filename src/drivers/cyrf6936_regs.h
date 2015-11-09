52f0859cc93c765de655e4a7abaf47b4eaf09406
Oliver Dippel <oliver@multixmedia.org>
2015-11-09 22:00:50 +0100

    geht ja mal garnet sowas, wer noch so stellen findet bitte bescheid geben

--- src/drivers/cyrf6936_regs.h
+++ src/drivers/cyrf6936_regs.h
@@ -1,22 +1,22 @@
 /*
-    This file is part of AutoQuad.
+	Copyright (C) 2013 Freek van Tienen <freek.v.tienen@gmail.com>
 
-    AutoQuad is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This file is part of paparazzi.
 
-    AutoQuad is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with AutoQuad.  If not, see <http://www.gnu.org/licenses/>.
+	paparazzi is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2, or (at your option)
+	any later version.
 
-    Copyright © 2014  Bill Nesbitt
+	paparazzi is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    Portions borrowed from the superbitrf project
-    Copyright (C) 2013 Freek van Tienen <freek.v.tienen@gmail.com>
+	You should have received a copy of the GNU General Public License
+	along with paparazzi; see the file COPYING.  If not, write to
+	the Free Software Foundation, 59 Temple Place - Suite 330,
+	Boston, MA 02111-1307, USA.
 */
 
 #ifndef _cyrf6936_regs_h
